46a0ea8f973f0cc67831211ba05374f83391b58e
l t <tuniklev@gmail.com>
2019-11-06 16:01:13 +0200

    completed_task

---
+++ proj_baloon_initial/Assets/Scripts/saveImage.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class saveImage : MonoBehaviour
+{
+
+    public int FileCounter = 0;
+
+    public void freezeFrame()
+    {
+        CamCapture();
+    }
+
+    void CamCapture()
+    {
+        Camera Cam = GetComponent<Camera>();
+
+        //change render target to our renderTexture
+        RenderTexture currentRT = RenderTexture.active;
+        RenderTexture.active = Cam.targetTexture;
+
+        Cam.Render();
+
+        Texture2D Image = new Texture2D(Cam.targetTexture.width, Cam.targetTexture.height);
+        Image.ReadPixels(new Rect(0, 0, Cam.targetTexture.width, Cam.targetTexture.height), 0, 0);
+        Image.Apply();
+
+        RenderTexture.active = currentRT;
+
+        var Bytes = Image.EncodeToPNG();
+        Destroy(Image);
+
+        File.WriteAllBytes(Application.dataPath + "/Frames/Frame_" + FileCounter + ".png", Bytes);
+        FileCounter++;
+    }
+
+}
