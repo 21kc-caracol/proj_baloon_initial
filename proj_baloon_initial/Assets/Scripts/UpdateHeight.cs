2d8f9e10e33b0de84a6848d95b2d71e17f27d14f
Eric <kiel.eric11@gmail.com>
2019-11-06 14:56:22 +0200

    Added height display

---
+++ proj_baloon_initial/Assets/Scripts/UpdateHeight.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class UpdateHeight : MonoBehaviour
+{
+    public GameObject Ball;
+    TMPro.TMP_Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TMPro.TMP_Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.SetText("Current height: " + Ball.transform.position.y);
+    }
+}
